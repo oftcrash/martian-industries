2a76b219d7b9d223c547d821dccf54af684b6e58
Kris Knowlton <kknowlton@hasbro.com>
2013-02-15 22:22:56 -0500

    clp and am now require cargo before producing cargo.

--- src/industries/automated_manufactory.py
+++ src/industries/automated_manufactory.py
@@ -5,14 +5,14 @@ def getInd():
 		"number" : "31",
 		"prod_cargo_types" : "SUPP",
 		"accept_cargo_types" : "ALOY, CHEM, PLST",
-		"prod_multiplier" : "15",
+		"prod_multiplier" : "0,0",
 		"min_cargo_distr" : "5",
 		"life_type" : "PROCESSING",
 		"random_sound_effects" : "0x03, 0x27",
 		"conflicting_ind_types" : "31",
-		"input_multiplier_1" : "0",
-		"input_multiplier_2" : "0",
-		"input_multiplier_3" : "0",
+		"input_multiplier_1" : "1,1",
+		"input_multiplier_2" : "1,1",
+		"input_multiplier_3" : "1,1",
 		"spec_flags" : "IND_FLAG_BUILT_NEAR_TOWN",
 		"prob_random" : "3",
 		"prob_in_game" : "2",
@@ -20,6 +20,14 @@ def getInd():
 		"fund_cost_multiplier" : "100",
 		"remove_cost_multiplier" : "0",
 		"tiles" : ["142", "143", "144", "145", "146", "147"],
-		"tile_cargos" : "[ALOY, 8],[CHEM, 8], [PLST, 8]"
+		"tile_cargos" : "[ALOY, 8],[CHEM, 8], [PLST, 8]",
+		"graphics" : '''
+			produce_cargo_arrival: am_produce;
+			produce_256_ticks: am_produce_256;
+			''',
+		"pre" : '''
+			produce (am_produce, waiting_cargo_1, waiting_cargo_2, waiting_cargo_3, 16, 16, 0);
+			produce (am_produce_256, 0, 0, 0, 0, 0, 0);
+			'''
 	}
 	return out
--- src/industries/colony_landing_pod.py
+++ src/industries/colony_landing_pod.py
@@ -5,18 +5,18 @@ def getInd():
 		"number" : "29",
 		"prod_cargo_types" : "SUPP, PASS",
 		"accept_cargo_types" : "DEUT",
-		"prod_multiplier" : "15, 15",
+		"prod_multiplier" : "0, 0",
 		"min_cargo_distr" : "5",
 		"life_type" : "PROCESSING",
 		"random_sound_effects" : "",
 		"conflicting_ind_types" : "",
-		"input_multiplier_1" : "0",
+		"input_multiplier_1" : "1, 2",
 		"input_multiplier_2" : "0",
 		"input_multiplier_3" : "0",
 		"spec_flags" : "",
 		"prob_random" : "3",
 		"prob_in_game" : "2",
-		"prospect_chance" : "0",
+		"prospect_chance" : "0.75",
 		"fund_cost_multiplier" : "100",
 		"remove_cost_multiplier" : "0",
 		"tiles" : ["137"],
@@ -38,9 +38,11 @@ def getInd():
 		},
 		"graphics" : '''
 			produce_cargo_arrival: clp_produce;
+			produce_256_ticks: clp_produce_256;
 		''',
 		"pre" : '''
-			produce (clp_produce, waiting_cargo_1, 0, 0, waiting_cargo_1, waiting_cargo_1);
+			produce (clp_produce, waiting_cargo_1, waiting_cargo_2, waiting_cargo_3, 16, 16, 0);
+			produce (clp_produce_256, 0, 0, 0, 0, 0, 0);
 		'''
 	}
 	return out
